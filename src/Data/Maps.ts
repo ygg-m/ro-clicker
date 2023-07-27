194eddbaebc6d0fa188b78a47700178211164c7b
Ygor Goulart <ygorgm95@gmail.com>
2023-07-27 02:20:11 -0300

    add: maps

---
+++ src/Data/Maps.ts
@@ -0,0 +1,14 @@
+export const mapList = [
+    {
+        id: 0,
+        nameID: "new_1-3",
+        name: "Training Ground",
+        monsterList: [
+            {
+                id: 1002,
+                quantity: 50,
+                respawnTime: 0,
+            }
+        ]
+    }
+]
