66c2d9bb899c97617054571337da3cdd54a06ff9
Ygor Goulart <ygorgm95@gmail.com>
2023-07-27 06:28:45 -0300

    add: persist options

--- src/Stores/page.ts
+++ src/Stores/page.ts
@@ -1,14 +1,15 @@
 import { create } from "zustand"
+import { persist } from "zustand/middleware";
 import { EquipmentSlotTypes } from "@/Types/Character/Equipment";
 import { ItemTypes } from "@/Types/Item";
-import {useRef} from "react"
+import { useRef } from "react";
 
 interface DetailWindowData {
   x: number;
   y: number;
-  equipData?: EquipmentSlotTypes,
-  itemData?: ItemTypes
-};
+  equipData?: EquipmentSlotTypes;
+  itemData?: ItemTypes;
+}
 
 interface State {
   // Items Detail Window
@@ -41,54 +42,61 @@ interface State {
   // Refs
 }
 
-const usePageStore = create<State>((set) => ({
-  // Item Details Window
-  detailWindows: [],
-  handleDetailWindow: (
-    e: React.MouseEvent,
-    equipData?: EquipmentSlotTypes,
-    itemData?: ItemTypes
-  ) => {
-    e.preventDefault();
-    const x = e.clientX;
-    const y = e.clientY;
-    const detailData: DetailWindowData = { x, y };
-    if (equipData) {
-      detailData.equipData = equipData;
-    } else if (itemData) {
-      detailData.itemData = itemData;
-    }
-    set((state) => ({ detailWindows: [...state.detailWindows, detailData] }));
-  },
-  handleCloseDetailWindow: (index: number) => {
-    set((state) => ({
-      detailWindows: state.detailWindows.filter((_, i) => i !== index),
-    }));
-  },
+const usePageStore = create<State>()(
+  persist(
+    (set) => ({
+      // Item Details Window
+      detailWindows: [],
+      handleDetailWindow: (
+        e: React.MouseEvent,
+        equipData?: EquipmentSlotTypes,
+        itemData?: ItemTypes
+      ) => {
+        e.preventDefault();
+        const x = e.clientX;
+        const y = e.clientY;
+        const detailData: DetailWindowData = { x, y };
+        if (equipData) {
+          detailData.equipData = equipData;
+        } else if (itemData) {
+          detailData.itemData = itemData;
+        }
+        set((state) => ({
+          detailWindows: [...state.detailWindows, detailData],
+        }));
+      },
+      handleCloseDetailWindow: (index: number) => {
+        set((state) => ({
+          detailWindows: state.detailWindows.filter((_, i) => i !== index),
+        }));
+      },
 
-  // Show Page Stuff
-  // Stats Above Main Player
-  isStatsAbovePlayerActive: false,
-  showStatsAbovePlayer: () => set({ isStatsAbovePlayerActive: true }),
-  hideStatsAbovePlayer: () => set({ isStatsAbovePlayerActive: false }),
-  toggleStatsAbovePlayer: () =>
-    set((state) => ({
-      isStatsAbovePlayerActive: !state.isStatsAbovePlayerActive,
-    })),
+      // Show Page Stuff
+      // Stats Above Main Player
+      isStatsAbovePlayerActive: false,
+      showStatsAbovePlayer: () => set({ isStatsAbovePlayerActive: true }),
+      hideStatsAbovePlayer: () => set({ isStatsAbovePlayerActive: false }),
+      toggleStatsAbovePlayer: () =>
+        set((state) => ({
+          isStatsAbovePlayerActive: !state.isStatsAbovePlayerActive,
+        })),
 
-  // Name Below Main Player
-  isNameBelowPlayerActive: false,
-  showNameBelowPlayer: () => set({ isNameBelowPlayerActive: true }),
-  hideNameBelowPlayer: () => set({ isNameBelowPlayerActive: false }),
-  toggleNameBelowPlayer: () =>
-    set((state) => ({
-      isNameBelowPlayerActive: !state.isNameBelowPlayerActive,
-    })),
+      // Name Below Main Player
+      isNameBelowPlayerActive: false,
+      showNameBelowPlayer: () => set({ isNameBelowPlayerActive: true }),
+      hideNameBelowPlayer: () => set({ isNameBelowPlayerActive: false }),
+      toggleNameBelowPlayer: () =>
+        set((state) => ({
+          isNameBelowPlayerActive: !state.isNameBelowPlayerActive,
+        })),
 
-  // Options Modal
-  isOptionsMenuOpen: false,
-  showOptionsMenu: () => set({ isOptionsMenuOpen: true }),
-  hideOptionsMenu: () => set({ isOptionsMenuOpen: false }),
-}));
+      // Options Modal
+      isOptionsMenuOpen: false,
+      showOptionsMenu: () => set({ isOptionsMenuOpen: true }),
+      hideOptionsMenu: () => set({ isOptionsMenuOpen: false }),
+    }),
+    { name: "options" }
+  )
+);
 
 export default usePageStore
